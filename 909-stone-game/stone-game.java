b2d9cbf10b7085d7c656005f20df8bb2bbd3c2ee
harshfi <86000141+harshfi@users.noreply.github.com>
2024-02-11 20:08:19 +0530

    Time: 8 ms (28.53%) | Memory: 44.3 MB (26.99%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,6 +1,20 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+
+        int dp[][] = new int [piles.length][piles.length];
+
+       return helper(0,piles.length-1, piles, dp)>0 ? true : false;
+
+       
         
+    }
+
+    public int helper (int i, int j, int p[], int dp [][]){
+
+        if(i>j) return 0;
+
+        if(dp[i][j]!=0) return dp[i][j];
+
+        return dp[i][j]=Math.max(p[i]- helper(i+1, j, p, dp), p[j]-helper(i,j-1,p,dp));
     }
 }
