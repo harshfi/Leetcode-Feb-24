a02cce1c80ec3c544edf3f8fbb5bdf471577153d
harshfi <86000141+harshfi@users.noreply.github.com>
2024-02-29 20:28:12 +0530

    Time: 10 ms (87.30%) | Memory: 65.9 MB (62.70%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -19,38 +19,34 @@ class Solution {
         Queue<TreeNode> st = new LinkedList<>();
         st.add(root);
         int level=0;
+        boolean even=true;
 
         while(!st.isEmpty()){
 
-            List<Integer> list = new ArrayList<>();
+           
             
             int size= st.size();
+            int prev = level%2==0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
 
            for(int i=0;i<size;i++){
-                TreeNode rn = st.remove();
-                list.add(rn.val);
-                if(level %2==0 && rn.val%2==0) return false;
-                if(level %2!=0 && rn.val%2!=0) return false;
+                TreeNode curr = st.remove();
+                
+                if(level%2==0 && (curr.val % 2 == 0 || curr.val <= prev))
+                    return false;
+                if(level%2!=0 && (curr.val % 2 == 1 || curr.val  >= prev))
+                    return false;
+
+                prev=curr.val;
                  
              
-                if(rn.left!=null) st.add(rn.left);
-                if(rn.right!=null) st.add(rn.right);
+                if(curr.left!=null) st.add(curr.left);
+                if(curr.right!=null) st.add(curr.right);
                
 
             }
-            // System.out.println(list+ " "+ level);
+            
 
-            for(int i=0;i<list.size()-1;i++){
-
-               
-                 for(int j=i+1;j<list.size();j++){
-
-                     if(level %2==0 &&  list.get(i)>=list.get(j) ) return false;
-                     
-                     else if(level %2!=0 && list.get(i)<=list.get(j)) return false;
-                   
-                 }
-            }
+            
             level++;
 
         }
