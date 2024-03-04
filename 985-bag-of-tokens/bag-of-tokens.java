2a84c0324a0edd54cada455144401a65a0f0ffac
harshfi <86000141+harshfi@users.noreply.github.com>
2024-03-04 16:20:43 +0530

    Time: 2 ms (97.13%) | Memory: 43 MB (23.57%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -1,40 +1,26 @@
 class Solution {
-    int max=0;
     public int bagOfTokensScore(int[] t, int p) {
-            
 
-        max=0;
         Arrays.sort(t);
-        int dp [][]= new int [t.length][t.length];
-        for(int i[] : dp){
-            Arrays.fill(i,-1);
-        }
 
         int i=0,j= t.length-1,s=0;
-       
-        find(i,j,s,p,t,dp);
+        int max=0;
+        while(i<t.length &&j>=0){
 
+            if(p>=t[i]){
+                s=s+1;
+                p-=t[i];
+                i++;
+            }
+            else if (s>=1){
+                s=s-1;
+                p=p+t[j];
+                j--;
+            }
+            else break;
+            max=Math.max(max,s);
+        }
         return max;
         
-    }
-    public void find(int i ,int j,int s,int p,int []t,int [][]dp){
-          max=Math.max(max,s);
-        if(i>=t.length  || j<0) return ;
-        
-         if(dp[i][j]!=-1) return ;
-      
-        if(p>=t[i]){
-           
-           find(i+1,j,s+1,p-t[i],t,dp);
-
-
-        }
-
-        if(s>=1){
-            find(i,j-1,s-1,p+t[j],t,dp);
-        }
-        find(i+1,j,s,p,t,dp);
-        dp[i][j]= max;
-
     }
 }
