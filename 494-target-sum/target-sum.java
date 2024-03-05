cca35a7e92f7a0f22eb7dc46fded780282568cb7
harshfi <86000141+harshfi@users.noreply.github.com>
2024-03-05 15:52:56 +0530

    Time: 219 ms (39.73%) | Memory: 44 MB (38.99%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,31 @@
 class Solution {
-    int cnt=0;
+    int tsum=0;
     public int findTargetSumWays(int[] nums, int target) {
-        cnt=0;
+        tsum=0;
+        for(int i=0;i<nums.length;i++) tsum+=nums[i];
+
+        int dp[][]= new int [tsum][nums.length];
+        for(int i[]:dp){
+            Arrays.fill(i,-1);
+        }
         
-        
-        return find(nums,target, 0,0);
+        return find(nums,target, 0,0,dp);
 
     }
-    public int find(int[] nums, int target , int i,int sum){
+    public int find(int[] nums, int target , int i,int sum,int dp[][]){
 
         if(i==nums.length){
             if(target-sum==0) return 1;
             return 0;
         }
-
-       return find(nums,target, i+1,sum-nums[i])+find(nums,target, i+1,sum+nums[i]);
+        
+        if(tsum-sum>=0 && tsum-sum< tsum && dp[tsum-sum][i]!=-1 ) return  dp[tsum-sum][i];
+        
+       int s= find(nums,target, i+1,sum-nums[i],dp)+find(nums,target, i+1,sum+nums[i],dp);
+       if(tsum-sum>=0 && tsum-sum< tsum){
+           dp[tsum-sum][i]=s;
+       }
+       return s;
         
 
     }
